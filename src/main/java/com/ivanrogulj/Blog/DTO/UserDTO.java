ba5d4df18af7124fac8c88e25e560e1a944f2f60
TusKabina <ivanrogulj248@gmail.com>
2023-09-07 01:28:51 +0200

    cleaned up userDTO

--- src/main/java/com/ivanrogulj/Blog/DTO/UserDTO.java
+++ src/main/java/com/ivanrogulj/Blog/DTO/UserDTO.java
@@ -1,10 +1,7 @@
 package com.ivanrogulj.Blog.DTO;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
-import com.ivanrogulj.Blog.Entities.Like;
 
-import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +9,5 @@ public class UserDTO {
     private Long id;
     private String username;
     private String fullName;
-//    @JsonManagedReference
-//    private List<Like> likes;
 
 }
