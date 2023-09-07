fc55c273b3e77c3b5e61e1ea2e3cfb10a074b987
TusKabina <ivanrogulj248@gmail.com>
2023-09-07 19:20:02 +0200

    cleaned up commentDTO

--- src/main/java/com/ivanrogulj/Blog/DTO/CommentDTO.java
+++ src/main/java/com/ivanrogulj/Blog/DTO/CommentDTO.java
@@ -4,21 +4,18 @@ package com.ivanrogulj.Blog.DTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String content;
     private UserDTO user;
-
-//    @JsonIgnore
-//    private PostDTO post;
     private LocalDateTime creationDate;
 }
