0ab27be31dc262a19922fb2f36ef395b6f3eaa87
TusKabina <ivanrogulj248@gmail.com>
2023-09-09 18:22:08 +0200

    moved API to backend

--- src/main/java/com/ivanrogulj/Blog/ExceptionHandler/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.ivanrogulj.Blog.ExceptionHandler;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
